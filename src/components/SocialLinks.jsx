a0b701c7951b981f271d8580fb4c6c1e62734373
SeaofDetritus <121258141+SeaofDetritus@users.noreply.github.com>
2025-04-30 10:57:28 -0500

    Create SocialLinks.jsx

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,15 @@
+export default function SocialLinks() {
+  return (
+    <nav className="social-links" aria-label="Social media">
+      <a href="https://facebook.com/metmuseum" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+        <img src="/icons/facebook.svg" alt="Facebook" />
+      </a>
+      <a href="https://twitter.com/metmuseum" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+        <img src="/icons/twitter.svg" alt="Twitter" />
+      </a>
+      <a href="https://instagram.com/metmuseum" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+        <img src="/icons/instagram.svg" alt="Instagram" />
+      </a>
+    </nav>
+  );
+}
